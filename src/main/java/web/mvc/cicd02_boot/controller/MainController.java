11bce711869dedea4f35cc503a350b356b801d12
hyowonsong <woniwoni30@gmail.com>
2025-07-09 12:44:59 +0900

    배포 자동화 테스트

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 CI/CD 배포자동화 re";
+        return "GitHub Actions를 이용한 CI/CD 배포자동화";
     }
 }
